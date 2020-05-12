d676c21bce785e751139ce8f6fb9b599ce90187a
Emanuele <em.zaccaria@gmail.com>
2020-05-12 17:08:07 +0200

    Modifica commenti

--- Step2/main.js
+++ Step2/main.js
@@ -44,64 +44,7 @@ $(document).ready(function() {
 
   };
 
-  // function printLineChart() {
-  //
-  //   $.ajax({
-  //     url : "server.php",
-  //     method : "GET",
-  //     success: function (myDataset) {
-  //       var ctx = $("#myChart");
-  //       var chart = new Chart(ctx, {
-  //         type: 'line',
-  //         data: {
-  //           labels: ['Gennaio','Febbraio','Marzo','Aprile','Maggio','Giugno','Luglio','Agosto','Settembre','Ottobre','Novembre','Dicembre'],
-  //           datasets: [{
-  //             label: 'Vendite',
-  //             backgroundColor: [
-  //               'rgba(255, 99, 132, 0.2)',
-  //               'rgba(54, 162, 235, 0.2)',
-  //               'rgba(255, 206, 86, 0.2)',
-  //               'rgba(75, 192, 192, 0.2)',
-  //               'rgba(153, 102, 255, 0.2)',
-  //               'rgba(255, 159, 64, 0.2)'
-  //             ],
-  //             borderColor: [
-  //               'rgba(255, 99, 132, 1)',
-  //               'rgba(54, 162, 235, 1)',
-  //               'rgba(255, 206, 86, 1)',
-  //               'rgba(75, 192, 192, 1)',
-  //               'rgba(153, 102, 255, 1)',
-  //               'rgba(255, 159, 64, 1)'
-  //             ],
-  //             data: myDataset,
-  //             borderWidth: 1
-  //           }]
-  //         },
-  //         // Configuration options go here
-  //         options: {
-  //           scales: {
-  //             yAxes: [{
-  //               ticks: {
-  //                 beginAtZero: true
-  //               }
-  //             }]
-  //           }
-  //         } //fine options
-  //
-  //       }); //fine charts
-  //     },
-  //     error : function (richiesta, stato, errore) {
-  //       alert("E' avvenuto un errore. " + errore);
-  //     }
-  //
-  //   }); //chiude ajax
-  //
-  // }; //chiude funzione printLineChart
-
-
   /////////////////////////////// ESECUZIONE
-
-  // printLineChart();
 
   var labelsMounth = ['Gennaio','Febbraio','Marzo','Aprile','Maggio','Giugno','Luglio','Agosto','Settembre','Ottobre','Novembre','Dicembre'];
 
@@ -116,9 +59,9 @@ $(document).ready(function() {
       var pieGraph = Object.keys(myDataset)[1];
       // la lista dei valori presenti in line
       var datasetsLine = myDataset.line;
-      // la lista delle keys presenti in pie
+      // la lista delle keys (quindi i labels) presenti in pie
       var labelsPie = Object.keys(myDataset.pie);
-      // la lista dei valori presenti in pie
+      // la lista dei values (quindi i dati del grafico) presenti in pie
       var datasetsPie = [];
       for (var values in myDataset.pie) {
         datasetsPie.push(myDataset.pie[values])
