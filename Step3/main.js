a6f31656efb1404d15dc1456134dfd1f8807af98
Emanuele <em.zaccaria@gmail.com>
2020-05-15 01:02:08 +0200

    Fine step 3

--- Step3/main.js
+++ Step3/main.js
@@ -92,46 +92,98 @@ $(document).ready(function() {
 
   // FUNZIONE
 
-  function printChartClevel(selector, typeOfGraph, mylabels, mysecondarylabel, myDataset) {
+  function printChartClevel(
+    selector,
+    typeOfGraph,
+    mylabels,
+    mysecondarylabel_1,
+    mysecondarylabel_2,
+    mysecondarylabel_3,
+    myDataset_1,
+    myDataset_2,
+    myDataset_3) {
     var ctx = $(selector);
     var chart = new Chart(ctx, {
       type: typeOfGraph,
       data: {
         labels: mylabels,
-        datasets: [{
-          label: mysecondarylabel,
+        datasets:
+        [
+          {
+            label: mysecondarylabel_1,
+            data: myDataset_1,
+            backgroundColor: [
+                'rgba(55, 99, 132, 0.2)',
+                'rgba(54, 162, 235, 0.2)',
+                'rgba(55, 206, 86, 0.2)',
+                'rgba(75, 192, 192, 0.2)',
+                'rgba(53, 102, 255, 0.2)',
+                'rgba(55, 159, 64, 0.2)'
+            ],
+            borderColor: [
+                'rgba(55, 99, 132, 1)',
+                'rgba(54, 162, 235, 1)',
+                'rgba(55, 206, 86, 1)',
+                'rgba(75, 192, 192, 1)',
+                'rgba(53, 102, 255, 1)',
+                'rgba(55, 159, 64, 1)'
+            ],
+            borderWidth: 1
+        },
+        {
+          label: mysecondarylabel_2,
+          data: myDataset_2,
           backgroundColor: [
-            'rgba(255, 99, 132, 0.2)',
-            'rgba(54, 162, 235, 0.2)',
-            'rgba(255, 206, 86, 0.2)',
-            'rgba(75, 192, 192, 0.2)',
-            'rgba(153, 102, 255, 0.2)',
-            'rgba(255, 159, 64, 0.2)'
+              'rgba(355, 99, 132, 0.2)',
+              'rgba(354, 162, 235, 0.2)',
+              'rgba(355, 206, 86, 0.2)',
+              'rgba(375, 192, 192, 0.2)',
+              'rgba(353, 102, 255, 0.2)',
+              'rgba(355, 159, 64, 0.2)'
           ],
           borderColor: [
-            'rgba(255, 99, 132, 1)',
-            'rgba(54, 162, 235, 1)',
-            'rgba(255, 206, 86, 1)',
-            'rgba(75, 192, 192, 1)',
-            'rgba(153, 102, 255, 1)',
-            'rgba(255, 159, 64, 1)'
+              'rgba(355, 99, 132, 1)',
+              'rgba(354, 162, 235, 1)',
+              'rgba(355, 206, 86, 1)',
+              'rgba(375, 192, 192, 1)',
+              'rgba(353, 102, 255, 1)',
+              'rgba(355, 159, 64, 1)'
           ],
-          data: myDataset,
           borderWidth: 1
-        }]
-      },
-      // Configuration options go here
+        },
+        {
+          label: mysecondarylabel_3,
+          data: myDataset_3,
+          backgroundColor: [
+              'rgba(5, 99, 132, 0.2)',
+              'rgba(4, 162, 235, 0.2)',
+              'rgba(5, 206, 86, 0.2)',
+              'rgba(5, 192, 192, 0.2)',
+              'rgba(3, 102, 255, 0.2)',
+              'rgba(5, 159, 64, 0.2)'
+          ],
+          borderColor: [
+              'rgba(5, 99, 132, 1)',
+              'rgba(4, 162, 235, 1)',
+              'rgba(5, 206, 86, 1)',
+              'rgba(5, 192, 192, 1)',
+              'rgba(3, 102, 255, 1)',
+              'rgba(5, 159, 64, 1)'
+          ],
+          borderWidth: 1
+        },
+      ]
+    },
       options: {
-        scales: {
-          yAxes: [{
-            ticks: {
-              beginAtZero: true
-            }
-          }]
-        }
-      } //fine options
-
-    }); //fine charts con opzioni attive
+          scales: {
+              yAxes: [{
+                  ticks: {
+                      beginAtZero: true
+                  }
+              }]
+          }
+      }
+  });
   }
 
   /////////////////////////////// ESECUZIONE
@@ -144,7 +196,6 @@ $(document).ready(function() {
     method : "GET",
     data: "level=" + getLevelFunction(),
     success: function (myDataset) {
-      console.log(myDataset);
       if (myDataset['levelAccessGuest']) {
         printChartGuest('#guest', myDataset['typeLineChart'], labelsMounth, 'Vendite', myDataset['dataLineChart']);
       }
@@ -152,7 +203,16 @@ $(document).ready(function() {
         printChartEmployee('#employee', myDataset['typePieEmployee'], myDataset['labelsPieEmployee'], myDataset['dataPieEmployee']);
       }
       if (myDataset['levelAccessClevel']) {
-        printChartClevel('#clevel', myDataset['typeLineClevel'], labelsMounth, myDataset['labelsLineClevel'], myDataset['dataLineClevel']);
+        printChartClevel('#clevel',
+         myDataset['typeLineClevel'],
+         labelsMounth,
+         myDataset['labelsLineClevel'][0],
+         myDataset['labelsLineClevel'][1],
+         myDataset['labelsLineClevel'][2],
+         myDataset['dataLineClevel'][0],
+         myDataset['dataLineClevel'][1],
+         myDataset['dataLineClevel'][2],
+       );
       }
     },
     error : function (richiesta, stato, errore) {
